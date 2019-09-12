e4f0ad58df9c07e3044d72f130d809715395e972
SongLiangChen <462039091@qq.com>
2019-09-12 17:13:00 +0800

    1

--- base_server_sdk/base_server_user/user.go
+++ base_server_sdk/base_server_user/user.go
@@ -1,7 +1,7 @@
 package base_server_user
 
 import (
-	"errors"
+	"github.com/becent/commom"
 	"github.com/becent/commom/base_server_sdk"
 	json "github.com/json-iterator/go"
 	"strconv"
@@ -28,13 +28,19 @@ type User struct {
 	Ext        string `json:"ext"`
 }
 
-var (
-	ErrInvalidParams = errors.New("params invalid or missed")
-	ErrServiceBusy   = errors.New("service busy")
-)
-
-// Register register a user to base_server_user
-func Register(user *User) (*User, error) {
+// Register 注册用户
+//
+// 1、orgId必须大于0
+// 2、account、phone、email至少要有一个有值
+// 3、account有值时，password必须有值
+// 4、code有值时，将会校验短信或者邮件验证码
+// 5、其他字段非必填
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1002 账户已经注册
+func Register(user *User, code string) (*User, *base_server_sdk.Error) {
 	params := make(map[string]string)
 	params["orgId"] = strconv.Itoa(user.OrgId)
 	params["phone"] = user.Phone
@@ -47,9 +53,10 @@ func Register(user *User) (*User, error) {
 	params["sex"] = strconv.Itoa(user.Sex)
 	params["birthDay"] = user.BirthDay
 	params["ext"] = user.Ext
+	params["code"] = code
 
-	if params["orgId"] == "" || (params["phone"] == "" && params["account"] == "" && params["email"] == "") || (params["account"] != "" && params["loginPwd"] == "") {
-		return nil, ErrInvalidParams
+	if params["orgId"] == "0" || (params["phone"] == "" && params["account"] == "" && params["email"] == "") || (params["account"] != "" && params["loginPwd"] == "") {
+		return nil, base_server_sdk.ErrInvalidParams
 	}
 
 	client := base_server_sdk.Instance
@@ -57,9 +64,537 @@ func Register(user *User) (*User, error) {
 	if err != nil {
 		return nil, err
 	}
-	if err = json.Unmarshal(data, user); err != nil {
-		return nil, ErrServiceBusy
+	if err := json.Unmarshal(data, user); err != nil {
+		common.ErrorLog("baseServerSdk_Register", params, "unmarshal user fail: "+string(data))
+		return nil, base_server_sdk.ErrServiceBusy
 	}
 
 	return user, nil
+}
+
+// LoginByAccount 通过账号进行登录
+//
+// 1、password有值时，会进行登录密码校验，为空则略过
+//
+// 异常返回：
+// 1003 账号不存在
+// 1004 用户已被冻结
+// 1005 密码错误
+func LoginByAccount(orgId int, account string, password string) (*User, *base_server_sdk.Error) {
+	if orgId == 0 || account == "" {
+		return nil, base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["account"] = account
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	data, err := client.DoRequest(client.Hosts.UserServerHost, "user", "loginByAccount", params)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &User{}
+	if err := json.Unmarshal(data, user); err != nil {
+		common.ErrorLog("baseServerSdk_LoginByAccount", params, "unmarshal user fail: "+string(data))
+		return nil, base_server_sdk.ErrServiceBusy
+	}
+
+	return user, nil
+}
+
+// LoginByPhone 通过手机号进行登录
+//
+// 1、code有值时，会进行短信校验，为空则略过
+// 2、password有值时，会进行登录密码校验，为空则略过
+//
+// 异常返回：
+// 1003 账号不存在
+// 1004 用户已被冻结
+// 1005 密码错误
+// 1008 短信验证码错误
+func LoginByPhone(orgId int, phone string, code string, password string) (*User, *base_server_sdk.Error) {
+	if orgId == 0 || phone == "" {
+		return nil, base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["phone"] = phone
+	params["code"] = code
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	data, err := client.DoRequest(client.Hosts.UserServerHost, "user", "loginByPhone", params)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &User{}
+	if err := json.Unmarshal(data, user); err != nil {
+		common.ErrorLog("baseServerSdk_LoginByPhone", params, "unmarshal user fail: "+string(data))
+		return nil, base_server_sdk.ErrServiceBusy
+	}
+
+	return user, nil
+}
+
+// LoginByEmail 通过手机号进行登录
+//
+// 1、code有值时，会进行邮件校验，为空则略过
+// 2、password有值时，会进行登录密码校验，为空则略过
+//
+// 异常返回：
+// 1003 账号不存在
+// 1004 用户已被冻结
+// 1005 密码错误
+// 1009 邮件验证码错误
+func LoginByEmail(orgId int, email string, code string, password string) (*User, *base_server_sdk.Error) {
+	if orgId == 0 || email == "" {
+		return nil, base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["email"] = email
+	params["code"] = code
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	data, err := client.DoRequest(client.Hosts.UserServerHost, "user", "loginByEmail", params)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &User{}
+	if err := json.Unmarshal(data, user); err != nil {
+		common.ErrorLog("baseServerSdk_LoginByEmail", params, "unmarshal user fail: "+string(data))
+		return nil, base_server_sdk.ErrServiceBusy
+	}
+
+	return user, nil
+}
+
+// GetUserInfo 获取用户信息
+//
+// 异常返回：
+// 1003 用户不存在
+func GetUserInfo(orgId int, userId int64) (*User, *base_server_sdk.Error) {
+	if orgId == 0 || userId == 0 {
+		return nil, base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+
+	client := base_server_sdk.Instance
+	data, err := client.DoRequest(client.Hosts.UserServerHost, "user", "getUserInfo", params)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &User{}
+	if err := json.Unmarshal(data, user); err != nil {
+		common.ErrorLog("baseServerSdk_LoginByEmail", params, "unmarshal user fail: "+string(data))
+		return nil, base_server_sdk.ErrServiceBusy
+	}
+
+	return user, nil
+}
+
+// GetBackLoginPwdByPhone 通过手机找回登录密码
+//
+// 1、code有值的话会进行短信校验，为空则忽略
+// 2、password是要设置的新密码
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1005 密码错误
+// 1008 短信验证码错误
+func GetBackLoginPwdByPhone(orgId int, phone, code, password string) *base_server_sdk.Error {
+	if orgId == 0 || phone == "" || password == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["phone"] = phone
+	params["code"] = code
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "getBackLoginPwd", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetBackLoginPwdByEmail 通过邮箱找回登录密码
+//
+// 1、code有值的话会进行邮箱校验，为空则忽略
+// 2、password是要设置的新密码
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1005 密码错误
+// 1009 邮箱验证码错误
+func GetBackLoginPwdByEmail(orgId int, email, code, password string) *base_server_sdk.Error {
+	if orgId == 0 || email == "" || password == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["email"] = email
+	params["code"] = code
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "getBackLoginPwd", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetBackTransPwdByPhone 通过手机找回支付密码
+//
+// 1、code有值的话会进行短信校验，为空则忽略
+// 2、password是要设置的新密码
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1005 密码错误
+// 1008 短信验证码错误
+func GetBackTransPwdByPhone(orgId int, phone, code, password string) *base_server_sdk.Error {
+	if orgId == 0 || phone == "" || password == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["phone"] = phone
+	params["code"] = code
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "getBackTransPwd", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetBackTransPwdByEmail 通过邮箱找回支付密码
+//
+// 1、code有值的话会进行邮箱校验，为空则忽略
+// 2、password是要设置的新密码
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1005 密码错误
+// 1009 邮箱验证码错误
+func GetBackTransPwdByEmail(orgId int, email, code, password string) *base_server_sdk.Error {
+	if orgId == 0 || email == "" || password == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["email"] = email
+	params["code"] = code
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "getBackTransPwd", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateLoginPwd 更新登录密码
+//
+// oldPassword有值则校验旧密码，为空则略过
+// newPassword必须有值
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1005 密码错误
+func UpdateLoginPwd(orgId int, userId int64, oldPassword, newPassword string) *base_server_sdk.Error {
+	if orgId == 0 || userId == 0 || newPassword == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+	params["oldPassword"] = oldPassword
+	params["newPassword"] = newPassword
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "updateLoginPwd", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateTransPwd 更新交易密码
+//
+// oldPassword有值则校验旧密码，为空则略过
+// newPassword必须有值
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1005 密码错误
+func UpdateTransPwd(orgId int, userId int64, oldPassword, newPassword string) *base_server_sdk.Error {
+	if orgId == 0 || userId == 0 || newPassword == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+	params["oldPassword"] = oldPassword
+	params["newPassword"] = newPassword
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "updateTransPwd", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// AuthRealName 实名认证
+//
+// idCard有值的话，会进行身份证校验，为空则忽略
+// 真实姓名为firstName+lastName
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1010 身份证认证失败
+func AuthRealName(orgId int, userId int64, firstName, lastName, idCard string) *base_server_sdk.Error {
+	if orgId == 0 || userId == 0 {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+	params["firstName"] = firstName
+	params["lastName"] = lastName
+	params["idCard"] = idCard
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "authRealName", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type Sex int
+
+const (
+	_ Sex = iota
+	Boy
+	Girl
+)
+
+type UserFields map[string]string
+
+func (u UserFields) SetNickName(nickName string) {
+	u["nickName"] = nickName
+}
+
+func (u UserFields) SetAvatar(avatar string) {
+	u["avatar"] = avatar
+}
+
+func (u UserFields) SetSex(sex Sex) {
+	u["sex"] = strconv.Itoa(int(sex))
+}
+
+func (u UserFields) SetBirthDay(birthDay string) {
+	u["birthDay"] = birthDay
+}
+
+func (u UserFields) SetExt(ext string) {
+	u["ext"] = ext
+}
+
+// UpdateUserInfo 更新用户信息
+//
+// 可以更新的字段为：
+// 1、nickName
+// 2、avatar
+// 3、sex
+// 4、birthDay
+// 5、ext
+//
+// 用例:
+// info := make(base_server_user.UserFields)
+// info.SetNickName("jak")
+// info.SetSex(base_server_user.Boy)
+//
+// base_server_user.UpdateUserInfo(1, 1000, info)
+//
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+func UpdateUserInfo(orgId int, userId int64, info UserFields) *base_server_sdk.Error {
+	if orgId == 0 || userId == 0 {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+	for key, val := range info {
+		params[string(key)] = val
+	}
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "updateUserInfo", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// BindAccount 绑定登录账号
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1006 重复绑定
+func BindAccount(orgId int, userId int64, account, password string) *base_server_sdk.Error {
+	if orgId == 0 || userId == 0 || account == "" || password == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+	params["account"] = account
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "bindAccount", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// BindPhone 绑定手机
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1006 重复绑定
+func BindPhone(orgId int, userId int64, phone, code string) *base_server_sdk.Error {
+	if orgId == 0 || userId == 0 || phone == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+	params["phone"] = phone
+	params["code"] = code
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "bindPhone", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// BindEmail 绑定邮箱
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1006 重复绑定
+func BindEmail(orgId int, userId int64, email, code string) *base_server_sdk.Error {
+	if orgId == 0 || userId == 0 || email == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+	params["email"] = email
+	params["code"] = code
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "bindEmail", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// AuthTransPwd 校验支付密码
+//
+// 异常返回：
+// 1000 服务繁忙
+// 1001 参数异常
+// 1003 用户不存在
+// 1005 密码错误
+func AuthTransPwd(orgId int, userId int64, password string) *base_server_sdk.Error {
+	if orgId == 0 || userId == 0 || password == "" {
+		return base_server_sdk.ErrInvalidParams
+	}
+
+	params := make(map[string]string)
+	params["orgId"] = strconv.Itoa(orgId)
+	params["userId"] = strconv.FormatInt(userId, 10)
+	params["password"] = password
+
+	client := base_server_sdk.Instance
+	_, err := client.DoRequest(client.Hosts.UserServerHost, "user", "authTransPwd", params)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ base_server_sdk/request.go
@@ -0,0 +1,186 @@
+package base_server_sdk
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/becent/commom"
+	"github.com/becent/commom/base_server_sdk/end"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	ErrServiceBusy = &Error{
+		Code:    900000,
+		Message: "service busy",
+	}
+	ErrHostEmpty = &Error{
+		Code:    900001,
+		Message: "host empty, please config it when do InitBaseServerSdk",
+	}
+	ErrInvalidParams = &Error{
+		Code:    900002,
+		Message: "params invalid or missed",
+	}
+)
+
+type Response struct {
+	Success bool        `json:"success"`
+	PayLoad interface{} `json:"payload"`
+	Err     *Error      `json:"error"`
+}
+
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (c *BaseServerSdkClient) DoRequest(host string, controller, action string, params map[string]string) ([]byte, *Error) {
+	if host == "" {
+		return nil, ErrHostEmpty
+	}
+
+	params["orgId"] = strconv.Itoa(c.OrgId)
+
+	var (
+		data []byte
+		err  error
+	)
+
+	if c.gRpcOnly {
+		data, err = c.doGRpcRequest(host, controller, action, params)
+	} else {
+		data, err = c.doHttpRequest(host, controller, action, params)
+	}
+	if err != nil {
+		common.ErrorLog("baseServerSdk_DoRequest", map[string]interface{}{
+			"host":       host,
+			"controller": controller,
+			"action":     action,
+			"params":     params,
+		}, err.Error())
+		return nil, ErrServiceBusy
+	}
+
+	resp := &Response{}
+	if err = json.Unmarshal(data, resp); err != nil {
+		common.ErrorLog("baseServerSdk_DoRequest", map[string]interface{}{
+			"host":       host,
+			"controller": controller,
+			"action":     action,
+			"params":     params,
+		}, err.Error())
+		return nil, ErrServiceBusy
+	}
+
+	if !resp.Success {
+		return nil, resp.Err
+	}
+
+	if data, err = json.Marshal(resp.PayLoad); err != nil {
+		common.ErrorLog("baseServerSdk_DoRequest", map[string]interface{}{
+			"host":       host,
+			"controller": controller,
+			"action":     action,
+			"params":     params,
+		}, err.Error())
+		return nil, ErrServiceBusy
+	}
+
+	return data, nil
+}
+
+func (c *BaseServerSdkClient) doHttpRequest(host string, controller, action string, params map[string]string) ([]byte, error) {
+	// Assembly body
+	var v url.Values
+	for key, val := range params {
+		v.Set(key, val)
+	}
+	body := strings.NewReader(v.Encode())
+
+	// Make new request
+	request, err := http.NewRequest("POST", strings.Join([]string{host, controller, action}, "/"), body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Fill header
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+	request.Header.Set("Signature", c.makeSignature())
+	request.Header.Set("RequestId", c.requestId())
+
+	// Do http request
+	resp, err := c.httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
+}
+
+func (c *BaseServerSdkClient) doGRpcRequest(host string, controller, action string, params map[string]string) ([]byte, error) {
+	// Make new request
+	request := &grpc_end.Request{
+		Controller: controller,
+		Action:     action,
+		Params:     params,
+		Header: map[string]string{
+			"requestId": c.requestId(),
+			"signature": c.makeSignature(),
+		},
+	}
+
+	// Get a gRpc conn from pool
+	pool := c.gRpcMapPool.GetPool(host)
+	conn, err := pool.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the request timeout
+	ctx, cancel := context.WithTimeout(context.Background(), c.requestTimeout)
+	defer cancel()
+
+	// Do gRpc request
+	resp, err := grpc_end.NewEndClient(conn.GetConn()).DoRequest(ctx, request)
+	if err != nil {
+		pool.DelErrorClient(conn)
+		return nil, err
+	}
+	_ = pool.Put(conn)
+
+	return resp.Data, nil
+}
+
+func (c *BaseServerSdkClient) requestId() string {
+	return common.Md5Encode(time.Now().String())
+}
+
+func (c *BaseServerSdkClient) makeSignature() string {
+	now := strconv.FormatInt(time.Now().Unix(), 10)
+
+	b := c.cp.Get().(*strings.Builder)
+	defer c.cp.Put(b)
+
+	b.WriteString(c.appId)
+	b.WriteString("-")
+	b.WriteString(now)
+	b.WriteString("-")
+	b.WriteString(c.appId)
+
+	data := b.String()
+
+	b.Reset()
+	b.WriteString(now)
+	b.WriteString(":")
+	b.WriteString(c.appId)
+	b.WriteString(":")
+	b.WriteString(common.Hmac(c.appSecretKey, data))
+
+	return b.String()
+}
