1efa435a294149e6668c7bf6b8d574e5f2d83356
mingzhong.li <mingzhong.li07@gmail.com>
2019-09-23 15:06:30 +0800

    add readme

--- base-server-sdk/base-server-octopus/example/main.go
+++ base-server-sdk/base-server-octopus/example/main.go
@@ -27,23 +27,23 @@ func main() {
 	}(now)
 
 	// email
-	//err := base_server_octopus.SendEmailCode(5, 1000, "850278995@qq.com", "zh")
+	//err := base_server_octopus.SendEmailCode(5, 1000, "xxx@qq.com", "zh")
 	//if err != nil {
 	//	println(err.String())
 	//}
 
 	// sim
-	//err := base_server_octopus.SendSimCode(5,1000, "13020082903", "zh")
-	//res, err := base_server_octopus.VerifySimCode(5,1000, "13020082903", "284770")
+	//err := base_server_octopus.SendSimCode(5,1000, "130xxxx1234", "zh")
+	//res, err := base_server_octopus.VerifySimCode(5,1000, "130xxxx1234", "284770")
 
 	// captcha
-	//res, err := base_server_octopus.InitCaptcha(5, "13020082903", "284770")
+	//res, err := base_server_octopus.InitCaptcha(5, "130xxxx1234", "284770")
 
 	// ga
-	//res, err := base_server_octopus.GenerateGa(5, "13020082903", "284770")
+	//res, err := base_server_octopus.GenerateGa(5, "130xxxx1234")
 
 	// idCard
-	res, err := base_server_octopus.AuthRealName(5, "李明忠q", "360702199107203312")
+	res, err := base_server_octopus.AuthRealName(5, "张三", "010203201909201234")
 
 	if err != nil {
 		println(err.String())
